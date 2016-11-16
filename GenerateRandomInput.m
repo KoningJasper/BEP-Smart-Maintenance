779c87ced4202c08ad195f6bc8efe01b21eba4fc
juliakortenhorst <juliakortenhorst@gmail.com>
2016-11-16 15:39:14 +0100

    Update GenerateRandomInput.m
    ID uit random input gehaald

--- GenerateRandomInput.m
+++ GenerateRandomInput.m
@@ -3,10 +3,9 @@ function [ random_input ] = GenerateRandomInput( tasks )
     %of the tasks.
     
     number_of_tasks = size(tasks, 1);
-    random_input = cell(number_of_tasks, 2);
+    random_input = cell(number_of_tasks,1);
     for i = 1:number_of_tasks
-        random_input{i, 1} = tasks{i,1};
-        random_input{i, 2} = rand();
+        random_input{i} = rand();
     end
 end
 
