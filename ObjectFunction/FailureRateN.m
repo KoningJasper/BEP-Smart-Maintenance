a39d004108f646f4cd815c0790498ea72c4c42d2
Jasper Koning <jasper@koning.io>
2016-11-16 14:29:05 +0100

    Edit FailureRateN function.

--- ObjectFunction/FailureRateN.m
+++ ObjectFunction/FailureRateN.m
@@ -1,13 +1,10 @@
-function [ failure_rate ] = FailureRate_N( step_size, step_no, weibull_beta,...
-    weibull_eta,weibull_m1,FailureRate_0)
-%Calculating the new failure rate after each time-step 
+function [ failure_rate ] = FailureRateN( step_size, weibull_beta, weibull_eta, m1, FailureRate_0)
+    %Calculating the new failure rate after each time-step 
 
-%To calculate the new failure rate a weibull-distribution of the failure
-%rate of the component in the general case is necesarry, providing beta, m1
-%and eta.
-
-FailureRate_N = FailureRate_0 + 1/weibull_m1*weibull_beta/weibull_eta*...
-    ((1/weibull_m1*(step_size))/weibull(eta))^(weibull_beta-1);
-
+    %To calculate the new failure rate a weibull-distribution of the failure
+    %rate of the component in the general case is necesarry, providing beta, m1
+    %and eta.
+    failure_rate = FailureRate_0 + 1/m1*weibull_beta/weibull_eta*...
+        ((1/m1*(step_size))/weibull(eta))^(weibull_beta-1);
 end
 
