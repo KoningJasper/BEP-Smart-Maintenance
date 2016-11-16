8ae9c85d4e46d75f8453102ee35a474e2f4f4a5f
Jasper Koning <jasper@koning.io>
2016-11-16 13:40:42 +0100

    Set t_p and t_max parameters.

--- Main.m
+++ Main.m
@@ -1,10 +1,16 @@
 % Clear
 clear all;
 
+% Parameters
+t_p   = 1; % Time Step (h)
+
 % Read data
 Components      = DataReader('Data/Components.xls');
 Tasks           = DataReader('Data/Tasks.xls');
 VesselLocations = DataReader('Data/VesselLocations.xls');
+
+% Set params according to data
+t_max = (length(VesselLocations) - 1) * t_p; % in h
 
 % Monte-Carlo
 for n = 1:100
