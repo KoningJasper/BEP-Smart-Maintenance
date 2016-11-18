864d66c2af90845196ee87c7b4d1b4e95f39439e
Jasper Koning <jasper@koning.io>
2016-11-18 13:32:27 +0100

    FailureRateN remove if.

--- ObjectFunction/FailureRateN.m
+++ ObjectFunction/FailureRateN.m
@@ -4,10 +4,6 @@ function [ failure_rate ] = FailureRateN( step_size, weibull_beta, weibull_eta, 
     %To calculate the new failure rate a weibull-distribution of the failure
     %rate of the component in the general case is necesarry, providing beta, m1
     %and eta.
-    if(m1 == 0)
-        failure_rate = FailureRate_0 + weibull_beta/weibull_eta*((step_size)/weibull_eta)^(weibull_beta-1);
-    else
-        failure_rate = FailureRate_0 + 1/m1*weibull_beta/weibull_eta*((1/m1*(step_size))/weibull_eta)^(weibull_beta-1);
-    end;
+    failure_rate = FailureRate_0 + 1/m1*weibull_beta/weibull_eta*((1/m1*(step_size))/weibull_eta)^(weibull_beta-1);
 end
 
