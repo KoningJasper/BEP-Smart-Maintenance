dfbe626c085cb2f92842b52825c7f63b29ad01c7
juliakortenhorst <juliakortenhorst@gmail.com>
2016-12-05 13:04:08 +0100

    Update Main.m
    ActualData ingevoegd

--- Main.m
+++ Main.m
@@ -8,13 +8,22 @@ clc;
 t_p     = 1;  % Time Step (h)
 no_runs = 50; % Number of MonteCarlo runs.
 
-% Read data
+% Read dummy data
 Components      = DataReader('Data/Components.xls');
 Tasks           = DataReader('Data/Tasks.xls');
 VesselLocations = DataReader('Data/VesselLocations.xls');
 
 % Set params according to data
 t_max = (length(VesselLocations) - 1) * t_p; % in h
+
+% Read actual data
+%t_max = 125*24; % in h
+%Components      = DataReader('ActualData/Components.xls');
+%Tasks           = DataReader('ActualData/Tasks.xls');
+%[VesselLocations] = VaarschemaMakerFunctie( t_max,t_p);
+
+
+
 
 %% Monte-Carlo %%
 % INIT %
