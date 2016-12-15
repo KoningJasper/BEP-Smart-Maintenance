c6f9f9a77f629df7c42752b47dc6ee25e4ea43aa
havix92 <harmenwpik@gmail.com>
2016-12-15 17:21:29 +0100

    Delete FindWeibullOfComponentByTaskId.m

--- Locaters/FindWeibullOfComponentByTaskId.m
+++
@@ -1,7 +0,0 @@
-function [ beta, eta ] = FindWeibullOfComponentByTaskId( task_id, tasks, components )
-    %FINDWEIBULLOFTASK Find the Weibull parameters of a task.
-    component = LocateComponentById(component_id, components);
-    beta      = component{1, 4};
-    eta       = component{1, 3};
-end
-
