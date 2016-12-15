b742e6b99ea55107082d4b519c0e2e0ffa05adf6
havix92 <harmenwpik@gmail.com>
2016-12-15 17:20:04 +0100

    Delete LocateComponentById.m

--- Locaters/LocateComponentById.m
+++
@@ -1,7 +0,0 @@
-function [row] = LocateComponentById( component_id, components )
-    component_ids = components(:, 1);
-    [rn, ~] = find([component_ids{:}] == component_id);
-    row = components(rn, :);
-end
-
-
